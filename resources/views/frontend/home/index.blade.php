c1cc59da919dcf0d20514666cfff396f38208ef5
IamCooller <cooller2000@inbox.ru>
2022-05-07 09:03:52 +0300

    Update index.blade.php

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -1509,7 +1509,7 @@
                             </div>
                             <div class="left-news__text">
                            
-                            {!! Str::words(strip_tags($el->text), 2, ' ...') !!}
+                            {!! Str::words(strip_tags($el->content), 2, ' ...') !!}
                             </div>
                             <div class="left-news__link left-news__link-f">
                                 <span class="about-body__link {{$test=$loop->index}}">
