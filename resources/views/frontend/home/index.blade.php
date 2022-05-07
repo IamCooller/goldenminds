937666963b3fb9b118cbf040b76c6c1007169337
IamCooller <cooller2000@inbox.ru>
2022-05-07 19:25:07 +0300

    fix

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -1509,7 +1509,7 @@
                             </div>
                             <div class="left-news__text">
                            
-                            {!! Str::words(strip_tags($el->content), 15, ' ...') !!}
+                            {!! Str::words(strip_tags($el->content), 13, ' ...') !!}
                             </div>
                             <div class="left-news__link left-news__link-f">
                                 <span class="about-body__link {{$test=$loop->index}}">
